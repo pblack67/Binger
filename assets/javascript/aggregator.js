fc39888cdd1e756a59154570da7247995cd133b4
Paul Black <pblack67@comcast.net>
2019-04-23 11:12:33 -0500

    Added show details tester

--- assets/javascript/aggregator.js
+++ assets/javascript/aggregator.js
@@ -1,6 +1,7 @@
 // Espisodate show search API endpoint
 var showSearchURL = "https://www.episodate.com/api/search?q=" // yourshowhere&page=1"
 var showSearchSuffix = "&page=1";
+var showSearchDetailsURL = "https://www.episodate.com/api/show-details?q="
 
 function searchShowButtonClicked() {
     console.log("searchShowButtonClicked");
@@ -28,6 +29,34 @@ function searchShowButtonClicked() {
     });
 }
 
+function searchShowDetailsButtonClicked() {
+    console.log("searchShowDetailsButtonClicked");
+    var show = $("#showDetailsText").val();
+    console.log("Searching for", show);
+    var myURL = showSearchDetailsURL + show;
+    $.get(myURL).then(function (response) {
+        console.log(response);
+        var resultOBJ = JSON.parse(response);
+        var resultText = JSON.stringify(resultOBJ, null, 2);
+
+        // var showText = "";
+        // resultOBJ.tv_shows.forEach(function(tvShow) {
+        //     showText += tvShow.name;
+        //     showText += " (";
+        //     showText += tvShow.network;
+        //     showText += ")\n"
+        // });
+
+        // resultText = showText + resultText;
+
+        console.log(resultText);
+        $("#searchShowDetailsResults").text(resultText);
+
+    });
+}
+
 $(function() {
     $("#searchShowButton").on("click", searchShowButtonClicked);
+    $("#searchShowDetailsButton").on("click", searchShowDetailsButtonClicked);
+
 });
